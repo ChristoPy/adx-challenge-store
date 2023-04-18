22c216afa69bdc1a6ffd0edf5a211336bca31a22
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-04-18 17:32:24 -0300

    Update server port to 8080.

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,4 +45,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  // Server configuration: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-server
+  server: {
+    port: 8080, // default: 3000
+  },
 }
