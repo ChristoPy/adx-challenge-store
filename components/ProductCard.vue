5603eda875fae284f73cf1c7e4331a8eb6c3baaa
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-04-19 14:25:27 -0300

    Stop pointing to product route.

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -11,12 +11,10 @@ defineProps<{
 
 <template>
   <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-    <RouterLink :to="`/produto/${product._id}`">
-      <ProductImage :name="product.name" :image="product.image" />
-      <h3 class="pt-3 font-bold text-2xl">{{ product.name }}</h3>
+    <ProductImage :name="product.name" :image="product.image" />
+    <h3 class="pt-3 font-bold text-2xl">{{ product.name }}</h3>
 
-      <p class="pt-1 pb-3 text-gray-900">{{ formatMoney(product.price) }}</p>
-    </RouterLink>
+    <p class="pt-1 pb-3 text-gray-900">{{ formatMoney(product.price) }}</p>
     <AddToCartButton :product="product" />
   </div>
 </template>
